7ab049ac7f885decf6618cf77cff8fccc59a9658
robertmri <56820682+robertmri@users.noreply.github.com>
2019-12-20 18:21:53 +0200

    Update UT_StringProblem.cs

--- String/UT_StringProblem.cs
+++ String/UT_StringProblem.cs
@@ -12,11 +12,7 @@ namespace matrix
         [TestCase("ab-cd", "dc-ba")]
         [TestCase("a-bC-dEf-ghIj", "j-Ih-gfE-dCba")]
         [TestCase("Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!")]
-        [TestCase("du5P@Bl2oCurILEgRY", "YR5g@EL2IruColBPud")]
-        [TestCase("@!!]]]", "@!!]]]")]
-        [TestCase("@!!ara]]]", "@!!ara]]]")]
-        [TestCase("z<*zj", "j<*zz")]
-        [TestCase("?6C40E", "?6E40C")]
+       
         public static void TestRevMethod(string input, string expected)
         {
             string current = StringsProblem.ReverseString(input);
